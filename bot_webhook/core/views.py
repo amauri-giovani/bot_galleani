ce02665a254f963a547028d69c11c620c2a165cb
Amauri Rossetti Giovani <amauri.giovani@gmail.com>
2024-12-10 16:37:18 -0300

    Changed github account

---
+++ bot_webhook/core/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from bot_webhook.core.utils import send_message
+
+
+def home(request):
+    return render(request, "index.html")
+
+
+@csrf_exempt
+def event(requests):
+    json_list = json.loads(requests.body)
+    # print(json_list)
+    first_name = json_list['message']['chat']['first_name']
+    chat_id = json_list['message']['chat']['id']
+    text_message = json_list['message']['text']
+    print(chat_id)
+    send_message('{}, sua mensagem ao contrário é\n{}'.format(first_name, text_message[::-1]), chat_id)
+    # return JsonResponse({'status': 'true', 'message': 'worked'})
+    return HttpResponse()
